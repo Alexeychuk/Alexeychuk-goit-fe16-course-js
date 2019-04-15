68512ef35536bb527fc2f2e6ccfe9e10ddd51b4f
Vladimir Alekseychuk <alekseychuk972mail.ru@gmail.com>
2019-04-15 21:48:21 +0300

    add 03

---
+++ module03/task_1/js/index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+function isLoginValid(login){
+  return login.length >= 4 && login.length <= 16;
+  
+}
+
+function isLoginUnique(allLogins, login){
+  return !allLogins.includes(login);
+}
+
+function addLogin(allLogins, login){
+  if(isLoginValid(login)){
+    if(isLoginUnique(allLogins, login)){
+      logins.push(login);
+      console.log('Логин успешно добавлен!');
+    }
+    else{
+      console.log('Такой логин уже используется!');
+    }
+  }
+  else{
+    console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+  }
+}
+
+
+addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
+addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
+addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
